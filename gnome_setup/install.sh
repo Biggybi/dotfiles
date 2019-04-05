6a6c8597c40d912ef295e311f35a7f1437ec52a0
biggybi <biggybi@gmail.com>
2019-04-05 23:26:58 +0200

    precision

--- gnome_setup/install.sh
+++ gnome_setup/install.sh
@@ -4,7 +4,7 @@ sudo apt autoremove
 
 sudo apt install git
 sudo apt install make
-sudo apt install autotools
+sudo apt install autotool
 sudo apt install gcc
 sudo apt install clang
 sudo apt install vim
@@ -12,6 +12,11 @@ sudo apt install vim
 sudo apt install gnome-session
 sudo apt install gnome-tweaks
 sudo apt install chrome-gnome-shell
+sudo update-alternatives --config gdm3.css
+
+sudo apt-get install libinput-tools
+sudo apt-get install xdotool
+sudo apt install xclip
 
 sudo snap install spotify
 
@@ -27,8 +32,8 @@ sudo cp -r themes/Materia-dark /usr/share/themes
 sudo cp -r themes/Materia-dark-compact /usr/share/themes
 
 #fusuma
+sudo gpasswd -a $USER input
 sudo snap install ruby
 gem install fusuma
 gsettings set org.gnome.desktop.peripherals.touchpad send-events enabled
 cp -r fusuma ~/.config
-
