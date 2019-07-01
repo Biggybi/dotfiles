44ca7d2af415f158214e47260c72cd2e10a38316
biggybi <biggybi@gmail.com>
2019-07-01 12:19:32 +0200

    vim installer finaly pushed\!\!\!

--- vim_install.sh
+++ vim_install.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+INSTALLDIR=/usr/share/vim/vim81
+VIMSOURCE=$HOME/dotfiles/programs/vim
+PYTHONCONF=/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu
+cd ~
+[ -d $VIMSOURCE ] || git clone https://github.com/vim/vim.git $VIMSOURCE
+cd $VIMSOURCE
+
+./configure --with-features=huge \
+	--enable-multibyte \
+	--enable-rubyinterp=yes \
+	--enable-python3interp=yes \
+	--with-python3-config-dir=/usr/lib/python3.5/config \
+	--enable-perlinterp=yes \
+	--enable-cscope \
+	--prefix=/usr/local
+
+make VIMRUNTIMEDIR=$INSTALLDIR
