d825dd4e043b3f5cc6f63dba5cdbe3141e27b768
biggybi <biggybi@gmail.com>
2019-08-08 02:44:53 +0200

    YCM install with semantic

--- youcompleteme_install.sh
+++ youcompleteme_install.sh
@@ -2,4 +2,5 @@ if [[ "$OSTYPE" == "darwin"* ]]
 then
 	brew install cmake
 fi
-python3 vim/bundle/YouCompleteMe/install.py
+# python3 vim/bundle/YouCompleteMe/install.py #without semantic for c family
+python3 vim/bundle/YouCompleteMe/install.py --clang-completer		#with semantc for c family
