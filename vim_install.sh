65c57bfd190a7d1a7fa30a48064f19f0b79b6bd6
Tris <biggybi@gmail.com>
2020-02-16 22:53:10 +0100

    vim 8.2

--- vim_install.sh
+++ vim_install.sh
@@ -1,12 +1,26 @@
+# **************************************************************************** #
+#                                                           LE - /             #
+#                                                               /              #
+#    vim_install.sh                                   .::    .:/ .      .::    #
+#                                                  +:+:+   +:    +:  +:+:+     #
+#    By: tris <tristan.kapous@protonmail.com>       +:+   +:    +:    +:+      #
+#                                                  #+#   #+    #+    #+#       #
+#    Created: 2020/02/16 20:09:29 by tris         #+#   ##    ##    #+#        #
+#    Updated: 2020/02/16 20:10:14 by tris        ###    #+. /#+    ###.fr      #
+#                                                          /                   #
+#                                                         /                    #
+# **************************************************************************** #
+
 #!/bin/bash
 
-INSTALLDIR=/usr/local/share/vim/vim81
+INSTALLDIR=/usr/local/share/vim/vim82
 VIMSOURCE=$HOME/dotfiles/programs/vim
 PYTHONCONF=/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu
 [ -d $VIMSOURCE ] || git clone https://github.com/vim/vim.git $VIMSOURCE
 cd $VIMSOURCE
 
-./configure --with-features=huge \
+./configure \
+	--with-features=huge \
 	--enable-multibyte \
 	--enable-rubyinterp=yes \
 	--enable-python3interp=yes \
@@ -18,3 +32,4 @@ cd $VIMSOURCE
 	--prefix=/usr/local
 
 make VIMRUNTIMEDIR=$INSTALLDIR
+sudo make install
