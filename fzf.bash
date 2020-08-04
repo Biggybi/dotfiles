d9a121145fc28b5b4826dc336d38f8720cea5b15
Tris <tris@tristankapous.com>
2020-08-05 00:12:16 +0200

    fzf path

--- fzf.bash
+++ fzf.bash
@@ -1,13 +1,15 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$DOT/vim/bundle/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$DOT/vim/bundle/fzf/bin"
+if [[ ! "$PATH" == *$DOT/vim/pack/bundle/start/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$DOT/vim/pack/bundle/start/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$DOT/vim/bundle/fzf/shell/completion.bash" 2> /dev/null
+# [[ $- == *i* ]] && source "$DOT/vim/bundle/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$DOT/vim/pack/bundle/start/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-[[ $- == *i* ]] && source "$DOT/vim/bundle/fzf/shell/key-bindings.bash" 2> /dev/null
+# [[ $- == *i* ]] && source "$DOT/vim/bundle/fzf/shell/key-bindings.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$DOT/vim/pack/bundle/start/fzf/shell/key-bindings.bash" 2> /dev/null
