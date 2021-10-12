fa17b4f43cba7a81e48af130afef16f9d8c2a42d
tris <tristan.kapous@protonmail.com>
2021-10-12 20:57:54 +0200

    zsh: pluginsourcer

---
+++ shells/zsh/plugins/zsh-pluginsourcer/zsh-pluginsourcer.zsh
@@ -0,0 +1,33 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                                              #
+#    zsh-pluginsourcer.zsh                                                     #
+#                                                                              #
+#    By: biggybi <biggybi@protonmail.com>                                      #
+#                                                                              #
+#    Created: 2021/10/12 20:01:09 by biggybi                                   #
+#    Updated: 2021/10/12 20:01:09 by biggybi                                   #
+#                                                                              #
+# **************************************************************************** #
+
+if [ -z "${PLUGINSOURCER_PATH}" ]
+then
+	if [ -n "${DOT}" ]
+	then
+		export PLUGINSOURCER_PATH="$DOT/shells/zsh/plugins"
+	else
+		export PLUGINSOURCER_PATH="$HOME/.zsh"
+	fi
+fi
+
+function zshSourcePlugins()
+{
+	for plugin in $(ls "$PLUGINSOURCER_PATH")
+	do
+		if [ "$plugin" != "zsh-pluginsourcer" ]
+		then
+			source "$PLUGINSOURCER_PATH/$plugin/$plugin.zsh"
+		fi
+	done
+}
+zshSourcePlugins
