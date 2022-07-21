0581590ee3a259295bf9dc1ac16449ee18e6aab7
biggybi <biggybi@protonmail.com>
2022-07-21 04:37:34 +0200

    feat(nvim): `telescope` config

--- nvim/plugin/config/telescope.lua
+++ nvim/plugin/config/telescope.lua
@@ -1,10 +1,16 @@
-local actions = require('telescope.actions')require('telescope').setup{
+local actions = require('telescope.actions')
+require('telescope').setup {
   defaults = {
     mappings = {
       i = {
         ["<c-j>"] = actions.move_selection_next,
-        ["<c-k>"] = actions.move_selection_previous
+        ["<c-k>"] = actions.move_selection_previous,
+        -- ["<c-b>"] = actions.move_forward,
+        -- ["<c-f>"] = actions.move_backward,
+        ["<Esc>"] = actions.close
       },
     },
+    file_ignore_patterns = { "node_modules", "%.out" },
+    prompt_prefix = "🔍 ",
   }
 }
